eb87371fe0535fe17ee1cb36ad01de029ee345cd
harsha8187-code <harsha8187@gmail.com>
2026-05-29 19:04:55 +0400

    Complete calculator logic

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,21 @@
 import numpy as np
 
 def calculate(list):
+if len(list) != 9:
+raise ValueError("List must contain some numbers.")
 
+    # Step 2: Convert the list into a 3x3 NumPy array
+    matrix = np.array(list).reshape(3, 3)
 
-
+    # Step 3: Calculate the required statistics
+    calculations = {
+        'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().item()],
+        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().item()],
+        'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().item()],
+        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().item()],
+        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().item()],
+        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().item()]
+    }
 
     return calculations
+
